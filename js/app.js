384defba1421ee9a5f86468c18a7af580a380de9
kubkowski <jakub.opara@gmail.com>
2014-08-29 21:30:34 +0200

    Finished lesson 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 App = Ember.Application.create();
 
+var Bookmark = Ember.Object.extend({
+	to_link: function(){
+		return "<a href='" + this.get("url") + "'>" + this.get("name") + "</a>";
+	}
+});
+Bookmark.reopen({
+	formatted: function(){
+		return this.get("name") + " - " + this.get("url");
+	}
+});
+
+var bookmark = Bookmark.create({ name:'tutsplus', url:'http://tutsplus.com' });
+
+
+
 App.Router.map(function() {
   // put your routes here
 });
