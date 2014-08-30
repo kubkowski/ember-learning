5d7048a106984faee26a051ff5cc13b874c840b1
kubkowski <jakub.opara@gmail.com>
2014-08-30 16:23:53 +0200

    Finished lessons 2 and 3

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 App = Ember.Application.create();
 
+var globalCounter = 0;
 var Bookmark = Ember.Object.extend({
 	to_link: function(){
 		return "<a href='" + this.get("url") + "'>" + this.get("name") + "</a>";
-	}
-});
-Bookmark.reopen({
+	},
 	formatted: function(){
 		return this.get("name") + " - " + this.get("url");
-	}
+	},
+	link: function(){
+		return this.to_link();
+	}.property("name", "url"),
+	updateCounter: Ember.observer(function(){
+		globalCounter += 1;
+		console.log("Global Counter has increased to: " + globalCounter);
+	}, "name")
 });
 
-var bookmark = Bookmark.create({ name:'tutsplus', url:'http://tutsplus.com' });
 
+var bookmark = Bookmark.create({ name:'tutsplus', url:'http://tutsplus.com' });
+bookmark.addObserver("url", function(){});
 
 
 App.Router.map(function() {
